74ddc9e16b2aaffca0caab869eead5ff98bbcd7d
Felipe Dolago <lagofelipe@gmail.com>
2020-07-29 00:02:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -20,10 +20,11 @@ def find_even_values(src)
 row_index = 0
 
 while row_index < src.count do
-
+if row_index % 2 == 0
   element_index = 0
 
   while element_index < src[row_index].count do
+if element_index % 2 == 0
     p src [row_index][element_index]
 
   element_index += 1
